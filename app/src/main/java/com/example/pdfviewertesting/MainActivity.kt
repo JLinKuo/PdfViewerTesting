0d18d70f833f0c40e96453df0e5c8feb49f5fea3
yaerse <yaerse@yahoo.com.tw>
2020-12-07 14:25:54 +0800

    [新增] 開啟檔案時，畫出浮水印

--- app/src/main/java/com/example/pdfviewertesting/MainActivity.kt
+++ app/src/main/java/com/example/pdfviewertesting/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.pdfviewertesting
 
 import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.os.Bundle
 import android.provider.OpenableColumns
 import android.widget.Button
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.graphics.scale
 import com.github.barteksc.pdfviewer.PDFView
 import java.io.File
 
@@ -17,13 +22,18 @@ class MainActivity : AppCompatActivity() {
 
     private val TAG = "MainActivity"
 
+    private val mainActivity by lazy { findViewById<ConstraintLayout>(R.id.main_activity_layout) }
     private val selectPDf by lazy { findViewById<Button>(R.id.main_activity_select_pdf_file) }
+    private val addASignArea by lazy { findViewById<Button>(R.id.main_activity_add_a_sign_name_area) }
     private val pdfViewer by lazy { findViewById<PDFView>(R.id.main_activity_pdfviewer) }
 
     private val dirPdfFolder by lazy {
         File(getExternalFilesDir(null), "$TEMP_SAVE_FILES_PATH")
     }
     private var pdfFileName = ""
+
+    private val minSignAreaWidth = 180
+    private val minSignAreaHeight = minSignAreaWidth / 2
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -64,10 +74,30 @@ class MainActivity : AppCompatActivity() {
         selectPDf.setOnClickListener {
             Tools.selectAnPdf(this, "Select PDF File")
         }
+
+        addASignArea.setOnClickListener {
+            addAnSignNameArea()
+        }
+    }
+
+    private fun addAnSignNameArea() {
+
+    }
+
+    private fun addAnWatermark(canvas: Canvas, pageWidth: Float, pageHeight: Float, zoom: Float) {
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.ic_launcher_round)
+        val scaleBitmap = bitmap.scale((bitmap.width * zoom).toInt(), (bitmap.height * zoom).toInt())
+        canvas.drawBitmap(scaleBitmap, (pageWidth - scaleBitmap.width) / 2, (pageHeight - scaleBitmap.height) / 2, Paint());
+        bitmap.recycle()
+        scaleBitmap.recycle()
+        pdfViewer.invalidate()
     }
 
     private fun openPDF(pdfFile: File) {
         pdfViewer.fromFile(pdfFile)
+                 .onDraw { canvas, pageWidth, pageHeight, zoom, displayedPage ->
+                     addAnWatermark(canvas, pageWidth, pageHeight, zoom)
+                 }
                  .load()
     }
 
